0925370ac3b7a603b08448139e8ffece680c4efd
Artem Krol <artemkrol47@gmail.com>
2023-07-11 11:43:05 +0200

    7/11 07 - Explicit wait for

--- library.js
+++ library.js
@@ -4,22 +4,60 @@ const driver = new Builder().forBrowser('chrome').build();
 
 driver.get('https://library-app.firebaseapp.com/');
 
-driver.findElement(By.css('input')).sendKeys('user123@gmail.com');
+var submitBtn = driver.findElement(By.css('body > main > section > div > div.col-md-2 > button'));
+driver.findElement(By.css('input')).sendKeys('user123');
 
-driver.findElement(By.css('body > main > section > div > div.col-md-2 > button')).click();
+driver.wait(function() {
+  return submitBtn.getCssValue('opacity')
+  .then(function(result) {
+    return result === '1';
+  });
+}, 15000).then(function() {
+  submitBtn.click();
+});
 
-driver.findElements(By.css('nav li'))
-  .then(function (elements) {
-    // Map and log the text of each element
-    elements.map(function (element) {
-      element.getText().then(function (txt) {
-        console.log('txt all menu : ' + txt)
-      });
-    });
+driver.wait(until.elementLocated(By.css('.alert-success')), 10000)
+  .then(function () {
+    return driver.findElement(By.css('.alert-success')).getText();
+  })
+  .then(function (txt) {
+    console.log('Alert success text is : ' + txt);
   })
   .catch(function (error) {
     // Handle any errors that occur
     console.log("Error: " + error);
-  });
+  })
+  
+
+
+
+// driver.wait(until.elementLocated(By.css('.alert-success')), 10000)
+//   .then(function () {
+//     return driver.findElement(By.css('.alert-success')).getText();
+//   })
+//   .then(function (txt) {
+//     console.log('Alert success text is : ' + txt);
+//   })
+//   .catch(function (error) {
+//     // Handle any errors that occur
+//     console.log("Error: " + error);
+//   })
+//   .finally(function () {
+//     driver.quit();
+//   });
+
+// driver.findElements(By.css('nav li'))
+//   .then(function (elements) {
+//     // Map and log the text of each element
+//     elements.map(function (element) {
+//       element.getText().then(function (txt) {
+//         console.log('txt all menu : ' + txt)
+//       });
+//     });
+//   })
+//   .catch(function (error) {
+//     // Handle any errors that occur
+//     console.log("Error: " + error);
+//   });
 
  
